edab374db14dee86dc874c7bdeb5235d99032d07
tyler <tyler@openraven.com>
2021-03-09 14:26:28 -0800

    added parquet support via pandas, delegated panda's types to have their own library.
    Signed-off-by: tyler <tyler@openraven.com>

---
+++ mockingbird/structured_data_document/panda_documents/__base.py
@@ -0,0 +1,53 @@
+#
+# Copyright 2021 Open Raven Inc. and the Mockingbird project authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from abc import abstractmethod
+
+import pandas
+
+from ..__base import __BaseStructuredDataType
+
+
+class __BasePandaDocument(__BaseStructuredDataType):
+    """
+    A wrapper for handling Panda DataFrame styled documents using Mockingbird.
+
+    The list of structured-data dictionaries found in __BaseStructuredDataType needs done in smaller chunks, or else
+    Pandas will crash converting it to a DataFrame.
+    """
+
+    @abstractmethod
+    def __init__(self, extension: str, config_file=None):
+        super().__init__(extension=extension, config_file=config_file)
+
+        self.chunk_size = 10000
+
+    def _get_data_frame(self):
+        structured_data = self._get_structured_data()
+
+        # chunk the list up into chunk_size lists
+        chunked_list = [structured_data[i:i + self.chunk_size] for i in range(0, len(structured_data), self.chunk_size)]
+        del structured_data
+
+        data_frame_list = []
+        # pop and delete off the list (for memory management), and converting them into DataFrames
+        while chunked_list:
+            head = chunked_list.pop()
+            new_data_frame = pandas.DataFrame.from_dict(head)
+            del head
+            data_frame_list.append(new_data_frame)
+
+        concatenated_df = pandas.concat(data_frame_list)
+        return concatenated_df
--- mockingbird/structured_data_document/panda_documents/parquet_document.py
+++ mockingbird/structured_data_document/panda_documents/parquet_document.py
@@ -16,25 +16,21 @@
 
 from typing import final
 
-import pandas
-import pandavro
-
-from .__base import __BaseStructuredDataType
+from ..panda_documents.__base import __BasePandaDocument
 
 
-class AvroDocument(__BaseStructuredDataType):
-    EXT = "avro"
+class ParquetDocument(__BasePandaDocument):
+
+    EXT = "parquet"
 
     def __init__(self, config_file=None):
-        super().__init__(extension=AvroDocument.EXT, config_file=config_file)
+        super().__init__(extension=ParquetDocument.EXT, config_file=config_file)
 
     @final
     def save(self, save_path: str) -> None:
-        save_file = self.setup_save_file(save_path=save_path, extension="avro")
+        save_file = self.setup_save_file(save_path=save_path, extension=self.extension)
 
-        structured_data = self._get_structured_data()
-
-        dataframe = pandas.DataFrame.from_dict(structured_data)
-        pandavro.to_avro(save_file, dataframe)
+        dataframe = self._get_data_frame()
+        dataframe.to_parquet(save_file)
 
         self._log_save(save_file)
