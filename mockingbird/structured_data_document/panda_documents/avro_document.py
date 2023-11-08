53d37e945fd84b2b989cccf3cbbd9927c5505b1d
Jeff Binder <jbinder@openraven.com>
2023-11-08 13:39:03 -0500

    Update requirements and switch avro package to avoid compatibility issues.
    Signed-off-by: Jeff Binder <jbinder@openraven.com>

--- mockingbird/structured_data_document/panda_documents/avro_document.py
+++ mockingbird/structured_data_document/panda_documents/avro_document.py
@@ -14,14 +14,35 @@
 # limitations under the License.
 #
 
+import json
+import numpy as np
+import avro.schema
+from avro.io import DatumWriter
+from avro.datafile import DataFileWriter
+
 from typing import final
 
-import pandavro
-
-from .__base import __BaseStructuredDataType
+from .__base import __BasePandaDocument
 
 
-class AvroDocument(__BaseStructuredDataType):
+AVRO_TYPES = {
+    np.dtype('?'): 'boolean',
+    np.int8: 'int',
+    np.int16: 'int',
+    np.int32: 'int',
+    np.uint8: {'type': 'int', 'unsigned': True},
+    np.uint16: {'type': 'int', 'unsigned': True},
+    np.uint32: {'type': 'int', 'unsigned': True},
+    np.int64: 'long',
+    np.uint64: {'type': 'long', 'unsigned': True},
+    np.dtype('O'): 'string',
+    np.unicode_: 'string',
+    np.float32: 'float',
+    np.float64: 'double',
+}
+
+
+class AvroDocument(__BasePandaDocument):
     EXT = "avro"
 
     def __init__(self, config_file=None):
@@ -32,6 +53,20 @@ class AvroDocument(__BaseStructuredDataType):
         save_file = self.setup_save_file(save_path=save_path, extension="avro")
 
         df = self._get_data_frame()
-        pandavro.to_avro(save_file, df)
+
+        schema = {
+            "type": "record",
+            "name": "TestData",
+            "fields": [
+                {'name': key, 'type': AVRO_TYPES[np_type]}
+                for key, np_type in df.dtypes.items()
+            ]
+        }
+
+        with open(save_file, 'wb') as f:
+            writer = DataFileWriter(f, DatumWriter(), avro.schema.parse(json.dumps(schema)))
+            for i, row in df.iterrows():
+                writer.append(row.to_dict())
+            writer.close()
 
         self._log_save(save_file)
